22ee874a60a7045c3b9b5ade2988929f87907618
fabianmontes <fabianmontes11fab@gmail.com>
2024-07-14 14:27:40 -0600

    first commit

---
+++ src/main/java/com/aluracursos/aluraforo/AluraforoApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.aluraforo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraforoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraforoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/aluraforo/AluraforoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.aluraforo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraforoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
